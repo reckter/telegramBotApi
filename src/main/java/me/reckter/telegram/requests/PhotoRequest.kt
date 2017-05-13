be0c7fb7f74735a1f9cfc6a0151736e9a2670dcb
Hannes <hannes@guedelhoefer.de>
2017-05-13 15:39:34 +0200

    :bug: with serialisation of photo sending

--- src/main/java/me/reckter/telegram/requests/PhotoRequest.kt
+++ src/main/java/me/reckter/telegram/requests/PhotoRequest.kt
@@ -9,19 +9,19 @@ import com.fasterxml.jackson.annotation.JsonProperty
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class PhotoRequest (
 
-    @JsonProperty("chat_id")
+    @field:JsonProperty("chat_id")
     val id: String,
 
     val photo: String,
 
     val caption: String? = null,
 
-    @JsonProperty("disable_notification")
+    @field:JsonProperty("disable_notification")
     val disableNotification: Boolean? = null,
 
-    @JsonProperty("reply_to_message_id")
+    @field:JsonProperty("reply_to_message_id")
     val replyTo: Int? = null,
 
-    @JsonProperty("reply_markup")
+    @field:JsonProperty("reply_markup")
     val replyMarkup: ReplyMarkup? = null
 )
